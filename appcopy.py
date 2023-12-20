063bebe4a8c82754ca22669658b4caba6c1c2ee4
Carnivurus <jmhernandezr2@gmail.com>
2023-12-20 12:44:54 -0600

    update

--- appcopy.py
+++ appcopy.py
@@ -10,55 +10,54 @@ car_data = pd.read_csv(
 st.header("Car Sales Dashboard")
 
 # Establecemos la configuración de todos nuestros filtros
-models = car_data["model"].unique()
 model_year = car_data["model_year"].unique()
-type_car = car_data["type"].unique()
-fuel = car_data["fuel"].unique()
-cylinders = car_data["cylinders"].unique()
+models = car_data["model"].unique()
+
 
 # Asignamos un boton que despliegue cada elemento y una variable que lo guarde
 selected_year = st.multiselect("Select the model year(s)", model_year)
-selected_fuel = st.multiselect("Select the type of fuel", fuel)
 selected_model = st.multiselect("Select the model(s)", models)
-selected_type = st.multiselect("Select the type of car", type_car)
-selected_cylinders = st.multiselect("Select the cylinders", cylinders)
 
-filter = car_data.query(
-    "@selected_year in model_year and\
-        @selected_fuel in fuel and\
-        @selected_model in model and\
-        @selected_type in type and\
-        @selected_cylinders in cylinders")
+if not selected_year:
+    selected_year = model_year
+
+if not selected_model:
+    selected_model = models
+
+df_filtered = car_data.query(
+    "@selected_year in model_year and @selected_model in model")
+
+st.text(selected_year)
+st.text(selected_model)
+st.dataframe(df_filtered)
 
 sales_per_model_button = st.checkbox("Show sales graphic per model")
 
 if sales_per_model_button:
-    fig = px.bar(filter, x="model", y="price")
-    st.plotly_chart(fig, use_container_width=True)
-else:
-    fig = px.bar(car_data, x="model", y="price")
+
+    fig = px.bar(df_filtered, x="model", y="price")
     st.plotly_chart(fig, use_container_width=True)
 
 
-hist_button = st.checkbox("Histograma")
-dist_button = st.checkbox("Dispersión")
+# hist_button = st.checkbox("Histograma")
+# dist_button = st.checkbox("Dispersión")
 
-if hist_button:  # al hacer click en el botón
-    st.write(
-        "Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
-    # Creación de un histograma
-    fig = px.histogram(car_data, x="odometer")
+# if hist_button:  # al hacer click en el botón
+#     st.write(
+#         "Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+#     # Creación de un histograma
+#     fig = px.histogram(car_data, x="odometer")
 
-    # mostrar un gráfico interactivo
-    st.plotly_chart(fig, use_container_width=True)
+#     # mostrar un gráfico interactivo
+#     st.plotly_chart(fig, use_container_width=True)
 
-if dist_button:
-    st.write(
-        "Se ha contruido una gráfica de dispersión"
-    )
-    fig2 = px.scatter(car_data, x="odometer", y="price")
+# if dist_button:
+#     st.write(
+#         "Se ha contruido una gráfica de dispersión"
+#     )
+#     fig2 = px.scatter(car_data, x="odometer", y="price")
 
-    st.plotly_chart(fig2, use_container_width=True)
+#     st.plotly_chart(fig2, use_container_width=True)
 
 
 # import streamlit as st
